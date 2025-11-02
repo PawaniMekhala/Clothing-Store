30ac4dd1afeeca037017649d53d3b9f13e68c80b
pawani mekhala <pawanimekhala3@gmail.com>
2025-11-03 01:44:48 +0530

    refactor: update upload.js file to upload wepg files

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,12 +1,12 @@
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinary.js';
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'profile_pictures', // folder in Cloudinary
-    allowed_formats: ['jpg', 'jpeg', 'png'],
+    folder: "product_images", // folder in Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
