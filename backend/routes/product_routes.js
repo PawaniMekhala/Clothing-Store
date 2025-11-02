e7c2b66b392580d2c46dfa134db49e3dfb6f0f24
pawani mekhala <pawanimekhala3@gmail.com>
2025-11-03 01:43:47 +0530

    feat: add route to create new product

--- backend/routes/product_routes.js
+++ backend/routes/product_routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { 
-  getAllProducts, 
-  getProductsByCategory, 
-  getProductById 
+import {
+  getAllProducts,
+  getProductsByCategory,
+  getProductById,
+  createProduct,
 } from "../controllers/product_controller.js";
+import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
@@ -15,5 +17,8 @@ router.get("/category/:categoryId", getProductsByCategory);
 
 // Get product details by product ID (e.g., /products/5)
 router.get("/:productId", getProductById);
+
+// POST /api/products  (multipart/form-data with field 'productImage')
+router.post("/add/newProduct", upload.single("productImage"), createProduct);
 
 export default router;
