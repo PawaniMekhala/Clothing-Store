c62797cdffbc23f4c243a2f4659e25ac40778902
PawaniMekhala <pawanimekhala3@gmail.com>
2025-11-02 22:47:38 +0530

    feat: add  user and product routes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
-
+import userRoutes from "./routes/user_routes.js";
+import productRoutes from "./routes/product_routes.js";
+import tempRoutes from "./routes/temporary_route.js";
 
 dotenv.config();
 const app = express();
@@ -10,12 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// routes
-
 // Define the root route
 app.get("/", (req, res) => {
   res.send("Welcome to the Clothing Store Backend API");
 });
-// app.use('/api/products', productRoutes);
+// user routes
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/temp", tempRoutes);
 
 export default app;
