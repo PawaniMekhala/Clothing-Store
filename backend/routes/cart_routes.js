e03897f875e234073043d1248e9067d4ed795eaa
pawani mekhala <pawanimekhala3@gmail.com>
2025-11-03 13:05:20 +0530

    feat: add route to remove product from cart

--- backend/routes/cart_routes.js
+++ backend/routes/cart_routes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { addToCart, viewCart } from "../controllers/cart_controller.js";
+import {
+  addToCart,
+  viewCart,
+  removeProductFromCart,
+} from "../controllers/cart_controller.js";
 import auth from "../middleware/auth_middleware.js";
 
 const router = express.Router();
 
 //add product to cart
 router.post("/add", auth, addToCart);
+//view cart
 router.get("/view", auth, viewCart);
+// remove item from cart
+router.delete("/remove-item/:productId", auth, removeProductFromCart);
 
 export default router;
