b00580863462d6c8efb34d66a245f26a735c4c7d
pawani mekhala <pawanimekhala3@gmail.com>
2025-11-03 14:19:20 +0530

    feat:add route for get all reviews about a project

--- backend/routes/review_routes.js
+++ backend/routes/review_routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { addReview } from "../controllers/review_controller.js";
+import {
+  addReview,
+  getAllReviewsByProduct,
+} from "../controllers/review_controller.js";
 import auth from "../middleware/auth_middleware.js";
 
 const router = express.Router();
 
 // add new review
 router.post("/add-review/:productId", auth, addReview);
+
+// Get all reviews for a product
+router.get("/get-all-reviews/:productId", getAllReviewsByProduct);
 
 export default router;
