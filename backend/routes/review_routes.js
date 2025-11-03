52f738987f20d18dcfd3631387bfa1579d0707b1
pawani mekhala <pawanimekhala3@gmail.com>
2025-11-03 15:26:09 +0530

    feat:add route to update review

--- backend/routes/review_routes.js
+++ backend/routes/review_routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   addReview,
   getAllReviewsByProduct,
+  updateReview,
 } from "../controllers/review_controller.js";
 import auth from "../middleware/auth_middleware.js";
 
@@ -12,5 +13,8 @@ router.post("/add-review/:productId", auth, addReview);
 
 // Get all reviews for a product
 router.get("/get-all-reviews/:productId", getAllReviewsByProduct);
+
+//update a review
+router.put("/update-review/:reviewId", auth, updateReview);
 
 export default router;
